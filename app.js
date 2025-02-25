5ae042f11f9dd9fc7d819481b1a0f2173bd90eb7
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-02-25 16:23:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/frontpage.html");
+})
+
+app.get("/greeting", (req, res) => {
+    const hour = new Date().getHours();
+    let greeting;
+
+    if (hour >= 5 && hour < 12) {
+        greeting = "Good Morning!";
+    } else if (hour >= 12 && hour < 18) {
+        greeting = "Good Afternoon!";
+    } else if (hour >= 18 && hour < 21){
+        greeting = "Good Evening!";
+    } else {
+        greeting = "Good Night!"
+    }
+
+    res.send({ data : greeting })
+
+})
+
+app.listen(PORT, () => { console.log(`Server is running on port:`, PORT) });
+//EOF
